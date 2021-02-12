b2cdf6dc006cef10cadc38a9512ade2999ac72f8
Tony <1332988826@qq.com>
2021-02-12 21:47:25 +0800

    pm2 config and robots.txt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
+      script: 'yarn',
       args: 'start',
     },
   ],
